b17112341f46a089955b1667cd1ecd775fdbfd9a
Moises Accorci de Mattos <moisesaccorci@gmail.com>
2022-07-09 12:36:21 -0300

    credentials updated

--- config/sequelizeCli.js
+++ config/sequelizeCli.js
@@ -1,6 +1,14 @@
 module.exports = {
     development: {
         dialect:'postgres',
+        host: 'localhost',
+        port: '5432',
+        database: 'ts-prototype_dev',
+        username: 'ts-prototype',
+        password: 'ts-prototype'
+    },
+    production: {
+        dialect:'postgres',
         host: process.env.HOST,
         port: '5432',
         database: process.env.DATABASE,
