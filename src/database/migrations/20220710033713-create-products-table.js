b73a26344f12cb95d6266f16045061830f800948
Moises Accorci de Mattos <moisesaccorci@gmail.com>
2022-07-10 01:04:46 -0300

    adding migrations

---
+++ src/database/migrations/20220710033713-create-products-table.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.DataTypes.TEXT
+      },
+      thumbnail_url: {
+        type: Sequelize.DataTypes.STRING
+      },
+      featured: {
+        defaultValue: false,
+        type: Sequelize.DataTypes.BOOLEAN
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.DataTypes.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT'
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('products')
+  }
+};
