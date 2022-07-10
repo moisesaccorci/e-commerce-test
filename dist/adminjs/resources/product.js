568434aeadba9902b6c3f86d07f5f73592537fe3
Moises Accorci de Mattos <moisesaccorci@gmail.com>
2022-07-10 18:20:26 -0300

    found bug on user_id name in cammel case. Now it's fixed

--- dist/adminjs/resources/product.js
+++ dist/adminjs/resources/product.js
@@ -8,10 +8,10 @@ const upload_1 = __importDefault(require("@adminjs/upload"));
 const path_1 = __importDefault(require("path"));
 exports.productResourceOptions = {
     navigation: 'Store',
-    editProperties: ['name', 'description', 'uploadThumbnail', 'featured', 'userId', 'price'],
-    filterProperties: ['name', 'description', 'featured', 'userId', 'created_at', 'updated_at', 'price'],
-    listProperties: ['id', 'name', 'featured', 'userId'],
-    showProperties: ['id', 'name', 'description', 'featured', 'thumbnail_url', 'userId', 'created_at', 'updated_at', 'price']
+    editProperties: ['name', 'description', 'uploadThumbnail', 'featured', 'user_id', 'price'],
+    filterProperties: ['name', 'description', 'featured', 'user_id', 'created_at', 'updated_at', 'price'],
+    listProperties: ['id', 'name', 'featured', 'user_id'],
+    showProperties: ['id', 'name', 'description', 'featured', 'thumbnail_url', 'user_id', 'created_at', 'updated_at', 'price']
 };
 exports.productResourceFeatures = [
     (0, upload_1.default)({
