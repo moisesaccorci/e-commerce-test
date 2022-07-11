079f3bdb439eb05325d2408ec5623300f7312968
Moises Accorci de Mattos <moisesaccorci@gmail.com>
2022-07-11 02:29:35 -0300

    routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,21 @@ import express from 'express'
 import { productsController } from './controllers/productsController'
 import { authController } from './controllers/authController'
 import { ensureAuth } from './middlewares/auth'
-
+const app = express()
 const router = express.Router()
+
+
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+
 router.post('/auth/register', authController.register)
 router.post('/auth/login', authController.login)
 
 router.get('/products', productsController.index)
 router.get('/products/:id', productsController.show)
-router.post('/products',productsController.create)
+router.post('/products', productsController.create)
 
 export { router }
