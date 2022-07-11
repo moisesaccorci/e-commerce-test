5535becb995a1de25ace4a638e629196015393dd
Moises Accorci de Mattos <moisesaccorci@gmail.com>
2022-07-10 21:54:54 -0300

    authorization header

--- dist/server.js
+++ dist/server.js
@@ -10,7 +10,7 @@ const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Headers", "Authorization");
     res.header("Access-Control-Request-Headers", "Content-Type");
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     app.use((0, cors_1.default)());
