ce8e61dcf5fded243775222d34c7b0259be949c5
Moises Accorci de Mattos <moisesaccorci@gmail.com>
2022-07-11 09:29:17 -0300

    final fixes

--- dist/server.js
+++ dist/server.js
@@ -12,7 +12,7 @@ app.use(express_1.default.urlencoded({ extended: true }));
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:4000");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
     // res.header("Access-Control-Allow-Headers", "Authorization, Content-Type")
